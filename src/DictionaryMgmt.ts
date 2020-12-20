2a6436f5a42f3acb94372536b0c2ec8ee825f810
hagay kassa <hagaykassa@gmail.com>
2020-12-21 00:26:51 +0200

    Creating DictionaryMgmt file

---
+++ src/DictionaryMgmt.ts
@@ -0,0 +1,43 @@
+import { makeObservable, observable, action } from "mobx";
+
+  export default class DictionaryMgmt {
+    words:Array<string>;
+    startWithCounter:number;
+    endWithCounter:number;
+    containCounter:number;
+    doubleCounter:number;
+    letterInDictionaryCounter:number;
+
+    constructor() {
+        makeObservable(this, {
+            startWithCounter: observable,
+            endWithCounter: observable,
+            containCounter: observable,
+            doubleCounter: observable,
+            letterInDictionaryCounter:observable,
+            updateWords: action
+        })
+        this.words = ["a", "ss", "sas", "dsa", "oro", "oaoa","salsa","arssaa","ttts"];
+        this.startWithCounter = 0;
+        this.endWithCounter = 0;
+        this.containCounter = 0;
+        this.doubleCounter = 0;
+        this.letterInDictionaryCounter=0;
+    }
+        updateWords(filterLetter:string|undefined) {
+            if(filterLetter){
+            this.containCounter =this.words.filter((word)=> { return word.indexOf(filterLetter) !== -1; }).length;
+            this.startWithCounter =this.words.filter((word)=> { return word.indexOf(filterLetter) === 0; }).length;
+            this.endWithCounter =this.words.filter((word)=> { return word.endsWith(filterLetter)}).length;
+            this.doubleCounter =this.words.filter((word)=> { return word.indexOf(filterLetter + filterLetter) !== -1; }).length; 
+            this.letterInDictionaryCounter=0;
+            this.words.forEach((word)=>{
+            this.letterInDictionaryCounter+=(word.match(new RegExp(filterLetter, "g")) || []).length;});
+            return true;
+          }
+          else return false;
+    }
+  }
+
+ 
+  
